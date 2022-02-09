f2beb980b64b5eac166b94ee919693a4054481ee
Vu <vutranvan.libra@gmail.com>
2022-02-09 12:59:50 +0700

    chore: refactor cpn account

---
+++ hyper/src/pages/account/AccountLayout.tsx
@@ -0,0 +1,56 @@
+// @flow
+import React, { useEffect, FC } from 'react'
+import { Container, Row, Col, Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
+
+// images
+import Logo from '../../assets/images/logo.png'
+
+type AccountLayoutProps = {
+  bottomLinks?: any
+}
+
+const AccountLayout: FC<AccountLayoutProps> = ({ bottomLinks, children }) => {
+  const { t } = useTranslation()
+
+  useEffect(() => {
+    if (document.body) document.body.classList.add('authentication-bg')
+
+    return () => {
+      if (document.body) document.body.classList.remove('authentication-bg')
+    }
+  }, [])
+
+  return (
+    <>
+      <div className="account-pages pt-2 pt-sm-5 pb-4 pb-sm-5">
+        <Container>
+          <Row className="justify-content-center">
+            <Col md={8} lg={6} xl={5} xxl={4}>
+              <Card>
+                {/* logo */}
+                <Card.Header className="pt-4 pb-4 text-center bg-primary">
+                  <Link to="/">
+                    <span>
+                      <img src={Logo} alt="" height="18" />
+                    </span>
+                  </Link>
+                </Card.Header>
+                <Card.Body className="p-4">{children}</Card.Body>
+              </Card>
+
+              {/* bottom links */}
+              {bottomLinks}
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <footer className="footer footer-alt">
+        {t('2018 - 2021 © Hyper - Coderthemes.com')}
+      </footer>
+    </>
+  )
+}
+
+export default AccountLayout
